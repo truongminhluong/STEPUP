531b5eb4fa505488c053de0c119ef79b54dd5875
Your Name <you@example.com>
2025-04-08 23:30:33 +0700

    Duong6203

---
+++ app/src/main/java/com/example/doan/SearchActivity.java
@@ -0,0 +1,52 @@
+package com.example.doan;
+
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    EditText etSearch;
+    TextView tvCancel;
+    ImageButton btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        etSearch = findViewById(R.id.etSearch);
+        tvCancel = findViewById(R.id.tvCancel);
+        btnBack = findViewById(R.id.btnBack);
+
+        List<String> shoes = Arrays.asList(
+                "Nike Air Max Shoes",
+                "Nike Jordan Shoes",
+                "Nike Air Force Shoes",
+                "Nike Club Max Shoes",
+                "Snakers Nike Shoes",
+                "Regular Shoes"
+        );
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new SearchAdapter(shoes));
+
+        btnBack.setOnClickListener(v -> finish());
+
+        tvCancel.setOnClickListener(v ->
+                Toast.makeText(this, "Cancel clicked", Toast.LENGTH_SHORT).show()
+        );
+    }
+}
