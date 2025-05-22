61746e95b2a9667ad59ae5ae751dc18244d626f1
khanhbqph <118644940+khanhbqph@users.noreply.github.com>
2025-05-22 17:32:09 +0700

    khanh navigationView

---
+++ app/src/main/java/com/example/doan/MainActivity.java
@@ -0,0 +1,226 @@
+package com.example.doan;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.bumptech.glide.Glide;
+import com.example.doan.Auth.SignInActivity;
+import com.example.doan.Screens.MyCartActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+    private DrawerLayout drawerLayout;
+    private BottomNavigationView bottomNavigationView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        hideSystemUI();
+        setupNavigation();
+        setupActionBar();
+        setupFab();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView tvName = headerView.findViewById(R.id.tv_user_name);
+        TextView tvHello = headerView.findViewById(R.id.tv_hello);
+        ImageView imgAvatar = headerView.findViewById(R.id.img_avatar);
+
+        tvName.setText("Alisson Becker");
+        tvHello.setText("Hey, 👋");
+
+        Glide.with(this)
+                .load("https://yourdomain.com/avatar.jpg")
+                .placeholder(R.drawable.avatar_sample)
+                .into(imgAvatar);
+
+        LinearLayout signOutLayout = findViewById(R.id.sign_out_container);
+        signOutLayout.setOnClickListener(v -> {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Xác nhận")
+                    .setMessage("Bạn có chắc chắn muốn đăng xuất không?")
+                    .setPositiveButton("Đăng xuất", (dialog, which) -> {
+                        // Đăng xuất Firebase
+                        FirebaseAuth.getInstance().signOut();
+
+                        // Đăng xuất Google nếu có dùng
+                        GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_SIGN_IN).signOut();
+
+                        // Chuyển về màn hình đăng nhập và xóa lịch sử stack
+                        Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    })
+                    .setNegativeButton("Hủy", null)
+                    .show();
+        });
+
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            NavController navController = ((NavHostFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.nav_host_fragment)).getNavController();
+
+            int id = item.getItemId();
+
+            if (id == R.id.nav_BestSeller) {
+                Toast.makeText(this, "Best Seller", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, BestSellerActivity.class);
+                startActivity(intent);
+            } else if (id == R.id.nav_home) {
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                bottomNavigationView.setSelectedItemId(R.id.homeFragment); // Fix quan trọng
+            } else if (id == R.id.nav_notifications) {
+                Toast.makeText(this, "Notification", Toast.LENGTH_SHORT).show();
+                bottomNavigationView.setSelectedItemId(R.id.notificationFragment);
+            } else if (id == R.id.nav_profile) {
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                bottomNavigationView.setSelectedItemId(R.id.profileFragment);
+            }
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
+    }
+
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+    }
+
+    private void setupNavigation() {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+            bottomNavigationView.setOnItemSelectedListener(item -> {
+                int itemId = item.getItemId();
+                String message = "";
+
+                if (itemId == R.id.homeFragment) {
+                    message = "Home clicked";
+                }else if (itemId == R.id.favoriteFragment) {
+                    message = "Favorite clicked";
+                }else if (itemId == R.id.notificationFragment) {
+                    message = "Notifications clicked";
+                } else if (itemId == R.id.profileFragment) {
+                    message = "Profile clicked";
+                }
+//                else if (itemId == R.id.cartFragment) {
+//                    message = "Cart clicked";
+//                }
+
+                if (!message.isEmpty()) {
+                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                }
+
+                // Vẫn điều hướng như mặc định
+                return NavigationUI.onNavDestinationSelected(item, navController);
+            });
+        } else {
+            throw new IllegalStateException("NavHostFragment not found. Check your XML layout.");
+        }
+    }
+
+
+    private void setupActionBar() {
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu); // icon dấu 4 chấm
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    private void setupFab() {
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, MyCartActivity.class);
+            Toast.makeText(this, "MyCart", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        MenuItem cartItem = menu.findItem(R.id.action_cart);
+        View actionView = cartItem.getActionView();
+        if (actionView != null) {
+            actionView.setOnClickListener(view -> {
+                Toast.makeText(this, "Giỏ hàng", Toast.LENGTH_SHORT).show();
+            });
+        }
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        } else if (id == R.id.action_cart) {
+            Toast.makeText(this, "Giỏ hàng", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/doan/Model/Product.java
@@ -0,0 +1,141 @@
+package com.example.doan.Model;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String code; //mã sản phẩm
+    private long price;
+    private String brand;
+    private String description; //Mô tả sản phẩm
+    private boolean status; //Trạng thái: còn hàng / hết hàng
+    private boolean hasVariation; //Có biến thể hay không
+    private int quantity; //Số lượng sản phẩm
+    private String imageUrl;
+    private boolean bestSeller;
+
+    private List<ProductVariant> variants;
+
+    public List<ProductVariant> getVariants() {
+        return variants;
+    }
+
+    public Product() {
+    }
+
+    public Product(String id, String name, String code, long price, String brand,
+                   boolean status, boolean hasVariation, int quantity,
+                   String imageUrl, boolean bestSeller, String description) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+        this.price = price;
+        this.brand = brand;
+        this.status = status;
+        this.hasVariation = hasVariation;
+        this.quantity = quantity;
+        this.imageUrl = imageUrl;
+        this.bestSeller = bestSeller;
+        this.description = description;
+    }
+
+    public Product(String id, String name, long price, String imageUrl, boolean bestSeller) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.bestSeller = bestSeller;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public boolean isHasVariation() {
+        return hasVariation;
+    }
+
+    public void setHasVariation(boolean hasVariation) {
+        this.hasVariation = hasVariation;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public boolean isBestSeller() {
+        return bestSeller;
+    }
+
+    public void setBestSeller(boolean bestSeller) {
+        this.bestSeller = bestSeller;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
